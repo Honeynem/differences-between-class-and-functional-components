5c8625319234616eb92bf2712c9cb7519f65917b
Honeynem <148876783+Honeynem@users.noreply.github.com>
2024-02-19 11:24:31 +0330

    some minor changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 //assigining class based components
 import { Component } from "react";
 
-class App extends Component {
-    render(){
-        return (<>
-            <h1>hello world</h1>
-            <h2>basics with react</h2>
-            </>)
-    }
+// class App extends Component {
+//     render(){
+//         return (<>
+//             <h1>hello world</h1>
+//             <h2>basics with react</h2>
+//             </>)
+//     }
+// }
+
+function App(){
+    return (<>
+         <h1>hello world</h1>
+        <h2>basics with react</h2>            
+         </>)
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // const element = <h1>hello world</h1>
 ReactDOM.render( <App/> , document.getElementById('root'));
